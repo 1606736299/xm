fbfccfb513371b9f65ac9575aa81e69b23112809
1741509527 <35564082+1741509527@users.noreply.github.com>
2018-03-20 11:12:21 +0800

    首页
    设置商品链接

--- laravel55/resources/views/admin/index.blade.php
+++ laravel55/resources/views/admin/index.blade.php
@@ -151,12 +151,19 @@
                     <li>
                         <a href="#"><i class="fa fa-edit"></i> <span class="nav-label">商品信息</span><span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
-                            <li><a class="J_menuItem" href="form_basic.html">基本表单</a>
+                            <!-- 修改的 -->
+                            <li><a class="J_menuItem" href="/admin/goods">产品详情</a>
+                            </li>
+                             <li><a class="J_menuItem" href="/admin/goodsinfo">添加规格</a>
+                            </li>
+                            <li><a class="J_menuItem" href="/admin/goodsinfo/create">查看规格</a>
+                            </li>
+                            <!-- <li><a class="J_menuItem" href="form_basic.html">基本表单</a>
                             </li>
                             <li><a class="J_menuItem" href="form_validate.html">表单验证</a>
                             </li>
                             <li><a class="J_menuItem" href="form_advanced.html">高级插件</a>
-                            </li>
+                            </li> -->
                             <li><a class="J_menuItem" href="form_wizard.html">表单向导</a>
                             </li>
                             <li>
